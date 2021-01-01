4e25fbd58bb83329af73a17cf84d7980f450dd5f
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2021-01-01 23:15:46 +0600

    heroku

--- studentjudge/settings.py
+++ studentjudge/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- studentjudge/urls.py
+++ studentjudge/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
 from django.urls import path,include
 
 from django.conf.urls.static import static
@@ -11,7 +13,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
     path('',include('accounts.urls')),
-
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
