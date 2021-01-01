24c7c76b01e1db7ed928a7fef2cbd6bc71a8f5d7
kamruzzaman leeon <48612426+kamruzzaman-leeon@users.noreply.github.com>
2021-01-01 23:29:29 +0600

    Update settings.py

--- studentjudge/settings.py
+++ studentjudge/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','studentjudge.herokuapp.com']
 
 # Application definition
 
